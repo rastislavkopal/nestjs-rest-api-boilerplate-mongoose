d67041061d2c9b17f2f0d7c323a307f364aded29
rasto <rasto.kopal@gmail.com>
2023-05-09 18:58:09 +0200

    added husky commit hooks

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,34 @@
 import {
-  // ClassSerializerInterceptor,
+  ClassSerializerInterceptor,
   ValidationPipe,
-  // VersioningType,
+  VersioningType,
 } from '@nestjs/common';
 import mongoose from 'mongoose';
 import { AppModule } from './app.module';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { useContainer } from 'class-validator';
+import { AllConfigType } from './config/config.type';
 import validationOptions from './utils/validation-options';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  const configService = app.get(ConfigService<AllConfigType>);
 
   app.enableShutdownHooks();
+  app.setGlobalPrefix(
+    configService.getOrThrow('app.apiPrefix', { infer: true }),
+    {
+      exclude: ['/'],
+    },
+  );
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   app.useGlobalPipes(new ValidationPipe(validationOptions));
-  // app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   const config = new DocumentBuilder()
     .setTitle('API')
@@ -27,7 +39,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  mongoose.set('debug', true); // TODO remove logging in deployment
+  mongoose.set(
+    // setup mongoose logger for dev mode only
+    'debug',
+    configService.getOrThrow<string>('app.nodeEnv', { infer: true }) ===
+      'development',
+  );
 
   await app.listen(3000);
 }
